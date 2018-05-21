cc0b576945b9c4ffc33d0294922ca17fc4f69f67
Julian Gruber <julian@juliangruber.com>
2018-05-21 10:48:12 +0200

    add prettier-standard

--- cli.js
+++ cli.js
@@ -5,12 +5,15 @@ process.name = 'browser'
 
 const launch = require('.')
 
-const [,, browser, uri] = process.argv
+const [, , browser, uri] = process.argv
 
 if (!browser || !uri) {
   console.error('Usage: just-launch BROWSER URI')
   process.exit(1)
 }
 
-launch(browser, { uri })
-.catch(err => setImmediate(() => { throw err }))
+launch(browser, { uri }).catch(err =>
+  setImmediate(() => {
+    throw err
+  })
+)
